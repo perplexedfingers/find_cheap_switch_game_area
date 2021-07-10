692666411db9d5e7fc590a55f70ffbab52cfcf24
Ray Zhu <14918698+perplexedfingers@users.noreply.github.com>
2021-07-10 08:54:49 +0800

    Convert functions into Julia

---
+++ Find.jl
@@ -0,0 +1,98 @@
+import HTTP
+import JSON
+import CodecZlib
+import TranscodingStreams
+
+
+const country_to_currency_conversion = Base.ImmutableDict(
+    "CA" => "CAD",
+    "MX" => "MXN",
+    "US" => "USD",
+    "DK" => "DKK",
+    "EE" => "EUR",
+    "FI" => "EUR",
+    "IE" => "EUR",
+    "LV" => "EUR",
+    "LT" => "EUR",
+    "NO" => "NOK",
+    "SE" => "SEK",
+    "GB" => "GBP",
+    "HR" => "EUR",
+    "CY" => "EUR",
+    "GR" => "EUR",
+    "IT" => "EUR",
+    "MT" => "EUR",
+    "PT" => "EUR",
+    "SI" => "EUR",
+    "ES" => "EUR",
+    "BG" => "EUR",
+    "CZ" => "CZK",
+    "HU" => "EUR",
+    "PL" => "PLN",
+    "RO" => "EUR",
+    "RU" => "RUB",
+    "SK" => "EUR",
+    "AT" => "EUR",
+    "BE" => "EUR",
+    "FR" => "EUR",
+    "DE" => "EUR",
+    "LU" => "EUR",
+    "NL" => "EUR",
+    "CH" => "CHF",
+    "AU" => "AUD",
+    "NZ" => "NZD",
+    "ZA" => "ZAR",
+    "JP" => "JPY",
+)
+
+
+function download_decompressed_gzip_json(url::String)::Dict
+    r = HTTP.get(url)
+    decompressed = transcode(CodecZlib.GzipDecompressor, r.body)
+    JSON.parse(String(decompressed))
+end
+
+
+function download_game_data()::Vector{Dict{String,Any}}
+    url = "http://eshop-checker.xyz/games.json"
+    result = download_decompressed_gzip_json(url)
+    [info["price"] for info in result["list"] if any(x -> length(values(x)) !== 0, info["price"])]
+end
+
+function download_currency_rate()::Dict{String,Any}
+    url = "http://eshop-checker.xyz/beta/statics/conv_rate.json"
+    result = download_decompressed_gzip_json(url)
+    result["rates"]
+end
+
+function compute(data::Vector{Dict{String,Any}}, rate::Dict{String,Any})
+    release_count = Dict([(area, 0) for area in Base.keys(country_to_currency_conversion)])
+    price_count = Dict([(area, 0) for area in Base.keys(country_to_currency_conversion)])
+    for game in data
+        for country in keys(game)
+            release_count[country] += 1
+        end
+
+        normalized_price = Dict([(area, price / rate[country_to_currency_conversion[area]])
+                            for (area, price) in pairs(game)])
+        _, country = findmin(normalized_price)
+        price_count[country] += 1
+    end
+    release_count, price_count
+end
+
+function main()
+    t_data = @async download_game_data()
+    t_rate = @async download_currency_rate()
+    data = fetch(t_data)
+    rate = fetch(t_rate)
+    release_count, price_count = compute(data, rate)
+
+    most_count, c_country = findmax(price_count)
+    _most_rate, r_country = findmax(Dict([(area, price_win / get(release_count, area, Inf)) for (area, price_win) in price_count]))
+    most_rate = round(_most_rate * 100, digits=2)
+
+    println("$c_country has $most_count games with good prices")
+    println("$r_country has $most_rate% of games with good prices")
+    println(release_count, price_count)
+end
